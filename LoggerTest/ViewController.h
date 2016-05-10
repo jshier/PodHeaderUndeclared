232828ba73bddff1afd26695bc6b9d3f9e17f016
Jon Shier <jon@jonshier.com>
2016-05-10 18:02:03 -0400

    Commit test project.

---
+++ LoggerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoggerTest
+//
+//  Created by Jon Shier on 5/10/16.
+//  Copyright © 2016 Jon Shier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+
+target 'LoggerTest' do
+  pod 'AFNetworkActivityLogger', :git => 'https://github.com/jshier/AFNetworkActivityLogger', :branch => "3_0_0"
+  pod 'AFNetworking'
+end
